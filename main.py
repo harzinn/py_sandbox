a9322864dc4fcf572d99304e268ae2e6aebbdc15
harzinn <nickkrugz@gmail.com>
2021-05-02 20:13:20 -0400

    chapter 5 homework

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 largest = None
 smallest = None
+inum = None
+
 while True:
     num = input("Enter a number: ")
     if num == "done" : break
-    print(num)
+    try:
+        inum = int(num)
+    except:
+        print('Invalid input')
+        continue
+    #print(inum)
+    if smallest is None:
+        smallest = inum
+        largest = inum
+    elif inum < smallest:
+        smallest = inum
+    elif inum > largest:
+        largest = inum
 
-print("Maximum", largest)
+
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
+

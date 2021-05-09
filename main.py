1d9b8d5f31507d8b5ff680de2b4e37c00548b25a
harzinn <nickkrugz@gmail.com>
2021-05-09 17:16:32 -0400

    chapter 7 homework setup

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-fhand = open('mbox-short.txt')
-print(fhand)
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File can not be opened:', fname)
+    quit()
+
+count = 0
+
+for line in fhand:
+    if line.startswith('Subject:')  :
+        count = count + 1
+
+print('There were', count, 'subject lines in', fname)

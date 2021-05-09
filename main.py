2b3a9ca7f181e6c7abfe97f0ba27dd9181f7b174
harzinn <nickkrugz@gmail.com>
2021-05-08 21:17:43 -0400

    start of files

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-email = 'nicholas.a.kruger2.ctr@mail.mil'
-
-# find the domain and print slice
-stpos = email.find('@')
-raw = email[stpos+1:]
-
-#remove leading and trailing whitespace if any
-final = raw.strip()
-
-print(final)
+fhand = open('mbox-short.txt')
+print(fhand)

0e9bbad45b574f01ceeda823b9bed0e7b768f9c1
harzinn <nickkrugz@gmail.com>
2021-05-10 21:32:14 -0400

    chapt7.2 done

--- homework/chapter7/chapter7_2.py
+++ homework/chapter7/chapter7_2.py
@@ -1,7 +1,21 @@
 # Use the file name mbox-short.txt as the file name
 fname = input("Enter file name: ")
 fh = open(fname)
+
+total = 0
+count = 0
+
 for line in fh:
     if not line.startswith("X-DSPAM-Confidence:") : continue
-    print(line)
-print("Done")
+
+    spos = line.find(':')
+    spam = line[spos+1:]
+
+    fspam = float(spam)
+
+    total = total + fspam
+    count = count + 1
+
+avg = total/count
+
+print('Average spam confidence:', avg)
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-# Use words.txt as the file name
+# Use the file name mbox-short.txt as the file name
 fname = input("Enter file name: ")
 fh = open(fname)
 
+total = 0
+count = 0
+
 for line in fh:
-    line = line.strip()
-    line = line.upper()
-    print(line)
+    if not line.startswith("X-DSPAM-Confidence:") : continue
+
+    spos = line.find(':')
+    spam = line[spos+1:]
+
+    fspam = float(spam)
+
+    total = total + fspam
+    count = count + 1
+
+avg = total/count
+
+print('Average spam confidence:', avg)

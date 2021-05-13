cf8628c619412db0f3439290d8f91ae726a021b2
harzinn <nickkrugz@gmail.com>
2021-05-12 21:36:56 -0400

    update docker scripts to automatically copy code and cleanup upon compile

--- docker/docker_compile.sh
+++ docker/docker_compile.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+exec 6>&1 # saves stdout
+echo "Copying files to local test staging"
+echo "..."
+exec > /dev/null  # redirect stdout to /dev/null
+cp -r ../ data/
+rm -rf data/docker/
+rm -rf data/.*
+exec 1>&6 6>&- # restore stdout
 echo "Compling python-sandbox"
 echo "..."
 docker build -t python-sandbox .
+echo "Cleaing up files"
+echo "..."
+rm -rf data/
+echo "Done! You can now use ./docker_run.sh"

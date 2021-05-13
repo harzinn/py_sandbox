1ac8fc9ef102e09c36ea3898886288df17bc543f
harzinn <nickkrugz@gmail.com>
2021-05-12 23:04:51 -0400

    .

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ fh = open(fname)
 searchTerm = input('What text are you looking for?: ')
 
 count = 0
+total = 0
 
 for line in fh:
+    total = total + 1
     if searchTerm not in line: continue
     count = count + 1
 
-print(searchTerm, 'occured', count, 'times in this file.')  
+print(searchTerm, 'occured', count, 'times in this file out of', total, 'lines')  

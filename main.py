342086da3b0cf6854f85150fc6c22a5f978e3a31
harzinn <nickkrugz@gmail.com>
2021-05-16 19:18:07 -0400

    chapter 8.4 prep

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-fname = input("Enter file name: ")
-fh = open(fname)
-searchTerm = input('What text are you looking for?: ')
-
-count = 0
-total = 0
-
-for line in fh:
-    total = total + 1
-    if searchTerm not in line: continue
-    count = count + 1
-
-print(searchTerm, 'occured', count, 'times in this file out of', total, 'lines')  
+fruit = 'Banana'
+fruit[0] = 'b'
+print(fruit)

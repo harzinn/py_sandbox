1f7a35eb2cc292cd930b255743a83ca2f5eceaa5
harzinn <nickkrugz@gmail.com>
2021-05-20 17:22:47 -0400

    homework 8_5 done

--- homework/chapter8/chapter8_5/chapter8_5.py
+++ homework/chapter8/chapter8_5/chapter8_5.py
@@ -4,5 +4,18 @@ if len(fname) < 1:
 
 fh = open(fname)
 count = 0
+selection = list()
 
+for line in fh:
+    line = line.rstrip()
+    if 'From' not in line : continue
+    if line.startswith('From: ') : continue
+    search = line.split()
+    if line.startswith('From'):
+        selection.append(search[1])
+        count = count + 1
+
+for email in selection:
+    print(email)
+    
 print("There were", count, "lines in the file with From as the first word")

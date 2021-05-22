c7491a00643cf8149669e066e1bbba33cc7d5622
harzinn <nickkrugz@gmail.com>
2021-05-22 19:08:35 -0400

    list comprehension practice

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-fhand = open('romeo.txt')
-counts = dict()
+#fname = input('Enter file name: ')
 
-for line in fhand:
-    words = line.split()
-    for word in words:
-        counts[word] = counts.get(word,0) + 1
+while True:
+    fname = input('Enter file name: ')
+    try:
+        handle = open(fname)
+        break
+    except:
+        print('Invalid file, try again')
+        continue
 
-lst = list()
+ls1 = list()
+dic1 = dict()
 
-for key, val in counts.items():
-    newtup = (val, key)
-    lst.append(newtup)
+for line in handle:
+    line = line.split()
+    for word in line:
+        ls1.append(word)
 
+for item in ls1:
+    dic1[item] = dic1.get(item, 0) + 1
 
+newls = [ (v,k) for k,v in dic1.items() ]
 
+newls.sort(reverse = True)
+
+print('The top 10 words are...')
+
+for item in newls[:10]:
+    print(item[1], 'occured', item[0], 'times!')

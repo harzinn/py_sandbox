32d0ef8995af3d6f61c990cbdbf82a7ff50954b0
harzinn <nickkrugz@gmail.com>
2021-05-26 13:57:45 -0400

    cleaned up homework 11 code

--- homework/chapter11/chapter11.py
+++ homework/chapter11/chapter11.py
@@ -1,8 +1,43 @@
-fname = input('Enter file name: ')
-if len(fname) < 1:
-    fname = 'regex_sum_42.txt'
+#import regular expressions
+import re
 
-fh = open(fname)
+#file check 
+while True:
+    fname = input('Enter file name or type \'done\' to quit: ' )
+    if len(fname) < 1:
+        fname = 'regex_sum_42.txt'
+    if fname == 'done' :
+        print('Quitting')
+        quit()
+    try:
+        fh = open(fname)
+        break
+    except:
+        print('Invalid filename, try again')
+        continue
 
+#run through the file line by line
+numbersList = list()
 for line in fh:
-    print(line)
+    line = line.rstrip()
+    numbersList.append(re.findall('[0-9]+', line))
+
+#flatten the list
+newList = list()
+for number in numbersList:
+    for item in number:
+        newList.append(item)
+
+#get our sum and count
+count = 0
+sum = 0
+for number in newList:
+    sum = sum + int(number)
+    count = count + 1
+
+print(count)
+print(sum)
+
+
+
+        

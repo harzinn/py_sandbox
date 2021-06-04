8c790e3ac7cbdb0616b29a7adaf38fc59ad17056
harzinn <nickkrugz@gmail.com>
2021-06-04 19:00:52 -0400

    pop setup

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import socket
+import urllib.request, urllib.parse, urllib.error
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
-mysock.send(cmd)
-
-while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
-        break
-    print(data.decode(),end='')
-
-mysock.close()
+fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
+for line in fhand:
+    print(line.decode().strip())
